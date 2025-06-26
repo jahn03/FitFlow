97a42b16e6eec46b32504f2622c8f344862de17e
jahn03 <jahnvisooknanan@gmail.com>
2025-06-26 14:17:58 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Test Message</Text>
+      <Image source={{ 
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300"}}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#160F29',
     alignItems: 'center',
     justifyContent: 'center',
   },
